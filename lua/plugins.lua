e45e8d7b834aa428a6edb0f2b11439756653877e
yapetrichka <yapetrichka@playflock.com>
2022-11-16 00:40:03 +0300

    style: Apply lua formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,45 +1,46 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup(function(use)
-		--Package Manager--
-use "wbthomason/packer.nvim"
-		--LSP, DAP, linters, formatters--
-use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-}
-		--Autocomplete--
-use {
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
-    "onsails/lspkind-nvim",
-}
-		--Syntax highlighting--
-use {
-    "nvim-treesitter/nvim-treesitter"
-}
-                --Color scheme--
-use "morhetz/gruvbox"
-		--File Explorer--
-use {
-  'nvim-tree/nvim-tree.lua',
-  requires = {
-    'nvim-tree/nvim-web-devicons', -- optional, for file icons
-  },
-  tag = 'nightly' -- optional, updated every week. (see issue #1193)
-}
-                --Finder--
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-		--Statusline--
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
-		--Bufferline--
-use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons'}
+	--Package Manager--
+	use "wbthomason/packer.nvim"
+	--LSP, DAP, linters, formatters--
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	}
+	--Autocomplete--
+	use {
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"onsails/lspkind-nvim",
+		"hrsh7th/vim-vsnip"
+	}
+	--Syntax highlighting--
+	use {
+		"nvim-treesitter/nvim-treesitter"
+	}
+	--Color scheme--
+	use "morhetz/gruvbox"
+	--File Explorer--
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		},
+		tag = 'nightly' -- optional, updated every week. (see issue #1193)
+	}
+	--Finder--
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		requires = { { 'nvim-lua/plenary.nvim' } }
+	}
+	--Statusline--
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+	}
+	--Bufferline--
+	use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'kyazdani42/nvim-web-devicons' }
 end)
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,13 +2,16 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 local on_attach = function(client, bufnr)
-    local opts = { noremap=true, silent=true }
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format{async = true}<CR>', opts)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format{async = true}<CR>', opts)
 end
 
-require("lspconfig").omnisharp.setup {
- on_attach = on_attach
-}
-require("lspconfig").sumneko_lua.setup{}
+local servers = { "sumneko_lua", "omnisharp" }
 
+for _, lsp in ipairs(servers) do
+	require("lspconfig")[lsp].setup {
+		on_attach = on_attach,
+		capabilities = capabilities
+	}
+end
